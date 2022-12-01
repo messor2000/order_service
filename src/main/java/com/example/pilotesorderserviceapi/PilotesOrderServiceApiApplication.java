29254bbd360f99dab10436e95e0782bb218be821
Ovcharenko Oleksandr <ovcharenko.messor@gmail.com>
2022-12-01 10:57:59 +0200

    init

---
+++ src/main/java/com/example/pilotesorderserviceapi/PilotesOrderServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pilotesorderserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotesOrderServiceApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PilotesOrderServiceApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/pilotesorderserviceapi/PilotesOrderServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pilotesorderserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotesOrderServiceApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
