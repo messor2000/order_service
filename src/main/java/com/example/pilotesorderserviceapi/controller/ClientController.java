f12f7261f758e7c159e68e11f15a532a21505529
Ovcharenko Oleksandr <ovcharenko.messor@gmail.com>
2022-12-02 17:59:38 +0200

    add endpoints in client controller

--- src/main/java/com/example/pilotesorderserviceapi/controller/ClientController.java
+++ src/main/java/com/example/pilotesorderserviceapi/controller/ClientController.java
@@ -1,9 +1,18 @@
 package com.example.pilotesorderserviceapi.controller;
 
+import com.example.pilotesorderserviceapi.dto.Client;
 import com.example.pilotesorderserviceapi.service.client.ClientService;
+import io.swagger.annotations.ApiOperation;
+import javax.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class ClientController {
   ClientService clientService;
+
+  @GetMapping(path = "/client/{id}")
+  @ApiOperation(value = "Get Board by ID")
+  public ResponseEntity<Client> getClient(@PathVariable Long id) {
+    Client client = clientService.getClientById(id);
+    return ResponseEntity.ok(client);
+  }
+
+  @PostMapping(path = "/client")
+  @ApiOperation(value = "Create new client")
+  public ResponseEntity<Client> createBoard(@RequestBody @Valid Client client) {
+    Client newClient = clientService.createClient(client);
+    return ResponseEntity.ok(newClient);
+  }
+
+  @DeleteMapping(path = "/client/{id}")
+  @ApiOperation(value = "Delete client by ID")
+  public void deleteClient(@PathVariable Long id) {
+    clientService.deleteClient(id);
+  }
 }
---
+++ src/test/java/com/example/pilotesorderserviceapi/controller/ClientControllerTest.java
@@ -0,0 +1,4 @@
+package com.example.pilotesorderserviceapi.controller;
+
+public class ClientControllerTest {
+}
